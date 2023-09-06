cf5684005f0467a7db32bdea8d36cd4d52f84053
quocminh1952 <minh195xyz@gmail.com>
2023-09-06 23:41:22 +0700

    add new lesson

---
+++ SpringBoot-Overview/05-CommandLine-demo/src/test/java/com/minh1952/springboot/start/strartSpringBoot/StrartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh1952.springboot.start.strartSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
